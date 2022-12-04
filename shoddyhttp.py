f530a4adb4f46b74c029bff5fea66984f3a777a4
MrFlyingPizza <65780477+MrFlyingPizza@users.noreply.github.com>
2022-12-04 13:13:36 -0800

    Separated server and handler and made handler handle multiple methods

--- shoddyhttp.py
+++ shoddyhttp.py
@@ -1,7 +1,7 @@
 import logging
 from enum import Enum
 from socket import socket, AF_INET, SOCK_STREAM
-from typing import Tuple, Dict, List
+from typing import Tuple, Dict
 
 HostAndPort = Tuple[str, str]
 
@@ -16,6 +16,7 @@ class HttpMethod(Enum):
     PUT = "PUT"
     HEAD = "HEAD"
     POST = "POST"
+    DELETE = "DELETE"
 
 
 class HttpVersion(Enum):
@@ -71,7 +72,7 @@ class HttpResponse:
         Builds the string of the status line portion of the response.
         :return: The parsed status line string.
         """
-        return f"{self.version.value} {self.status.value} {HttpStatusMessage[self.status.name].value}\n"
+        return f"{self.version.value} {self.status.value} {HttpStatusMessage[self.status.name].value}\r\n"
 
     def __build_headers(self) -> str:
         """
@@ -79,17 +80,17 @@ class HttpResponse:
         If there are no headers, then the empty string is returned.
         :return: The parsed headers string.
         """
-        if self.headers is None:
+        if self.headers is None or len(self.headers) <= 0:
             return ""
 
-        return "\n".join(f"{key}: {value}" for key, value in self.headers.items()) + "\n"
+        return "\r\n".join(f"{key}: {value}" for key, value in self.headers.items()) + "\r\n"
 
     def to_raw(self) -> str:
         """
         Builds the entire HTTP response string.
         :return: The parsed HTTP response string.
         """
-        return f"{self.__build_status_line()}{self.__build_headers()}\n{self.data}"
+        return f"{self.__build_status_line()}{self.__build_headers()}\r\n{self.data}"
 
 
 class HttpRequest:
@@ -118,7 +119,7 @@ class HttpRequest:
         Builds the string of the request line portion of this HTTP request.
         :return: The parsed request line string.
         """
-        return f"{self.method.value} {self.url} {self.version.value}\n"
+        return f"{self.method.value} {self.url} {self.version.value}\r\n"
 
     def __build_headers(self):
         """
@@ -126,16 +127,17 @@ class HttpRequest:
         If there are no headers, then the empty string is returned.
         :return: The parsed headers string.
         """
-        if self.headers is None:
+        if self.headers is None or len(self.headers) <= 0:
             return ""
-        return "\n".join(f"{key}: {value}" for key, value in self.headers.items()) + "\n"
+
+        return "\r\n".join(f"{key}: {value}" for key, value in self.headers.items()) + "\r\n"
 
     def to_raw(self):
         """
         Builds the entire HTTP request string.
         :return: The parsed HTTP request string.
         """
-        return f"{self.__build_request_line()}{self.__build_headers()}\n{self.body}"
+        return f"{self.__build_request_line()}{self.__build_headers()}\r\n{self.body}"
 
 
 def http_request_from_raw(raw_request: str) -> HttpRequest:
@@ -146,8 +148,8 @@ def http_request_from_raw(raw_request: str) -> HttpRequest:
     """
     top_section: str
     body: str
-    top_section, body = raw_request.split("\n\n", maxsplit=1)
-    top_lines = top_section.split("\n")
+    top_section, body = raw_request.split("\r\n\r\n", maxsplit=1)
+    top_lines = top_section.split("\r\n")
 
     method, url, version = top_lines[0].split(" ")
     header_lines = top_lines[1:]
@@ -160,64 +162,183 @@ def http_request_from_raw(raw_request: str) -> HttpRequest:
     return HttpRequest(method=HttpMethod(method), url=url, version=HttpVersion(version), headers=headers, body=body)
 
 
+class HttpResponses:
+    """
+    A class for holding typical responses that can be used repeatedly, like Request Timed Out or Not Found.
+    """
+    TIMEOUT = HttpResponse(status=HttpStatusCode.REQUEST_TIMED_OUT)
+    TIMEOUT_BYTES = TIMEOUT.to_raw().encode()
+
+    NOT_FOUND = HttpResponse(status=HttpStatusCode.NOT_FOUND)
+    NOT_FOUND_BYTES = NOT_FOUND.to_raw().encode()
+
+
+class ConnectionHandler:
+    """
+    An instance of this class handles one open HTTP connection. The instance is invalidated if the connection closes.
+    """
+
+    def __init__(self, connection: socket, address: HostAndPort, timeout=5):
+        """
+        The open connection to handle the request using.
+        :param connection: The socket of the open connection.
+        :param timeout: How long to wait for data before sending timeout and closing connection.
+        """
+        self.connection = connection
+        self.address = address
+        self.timeout = timeout
+
+    def __receive_request(self, conn: socket) -> HttpRequest | None:
+        """
+        Receive all data from a connection, and send timeout the connection if no activity for too long.
+        :param conn: Socket connection to receive data from.
+        :return: All bytes of the data received. None if request timeout.
+        """
+        chunks = []
+        receiving = True
+        while receiving:
+            data = None
+            try:
+                conn.settimeout(self.timeout)
+                data = conn.recv(2048)
+                conn.settimeout(None)
+
+                chunks.append(data)
+
+            except TimeoutError:
+                conn.settimeout(None)
+                conn.send(HttpResponses.TIMEOUT_BYTES)
+                conn.close()
+
+            if not data:
+                receiving = False
+
+        if len(chunks) <= 0:
+            return None
+
+        s = b''.join(chunks).decode()
+
+        logging.info(f"Received:\n{s}\nfrom:\n{self.address}")
+
+        return http_request_from_raw(s)
+
+    def __handle_get_request(self, request: HttpRequest) -> HttpResponse:
+        """
+        Handles an HTTP request with the GET method.
+        It gets a file from the file system indicated by the URL path and sends it back.
+        Send 404 Not Found if not found by the path.
+        :param request: The request object.
+        :return: An HTTP response to send back to the client.
+        """
+        if request.method != HttpMethod.GET:
+            raise f"GET request handler cannot handle request of {request.method}"
+
+        # TODO: implement
+        ...
+
+    def __handle_put_request(self, request: HttpRequest) -> HttpResponse:
+        """
+        Handles an HTTP request with the PUT method.
+        It should replace a file specified by the URL path.
+        Send 404 Not Found if not found by the path.
+        :param request: The request object.
+        :return: An HTTP response to send back to the client.
+        """
+        if request.method != HttpMethod.PUT:
+            raise f"PUT request handler cannot handle request of {request.method}"
+
+        # TODO: implement
+        ...
+
+    def __handle_post_request(self, request: HttpRequest) -> HttpResponse:
+        """
+        Handles an HTTP request with the POST method.
+        It should save the contents of the body to the file system.
+        It should send 400 Bad Request if a file already exists by the given path.
+        :param request: The request object.
+        :return: An HTTP response to send back to the client.
+        """
+        if request.method != HttpMethod.POST:
+            raise f"POST request handler cannot handle request of {request.method}"
+
+        # TODO: implement
+        ...
+
+    def __handle_head_request(self, request: HttpRequest) -> HttpResponse:
+        """
+        Handles an HTTP request with the HEAD method.
+        :param request: The request object.
+        :return: An HTTP response to send back to the client.
+        """
+        if request.method != HttpMethod.HEAD:
+            raise f"HEAD request handler cannot handle request of {request.method}"
+
+        # TODO: implement
+        ...
+
+    def __handle_delete_request(self, request: HttpRequest) -> HttpResponse:
+        """
+        Handles an HTTP request with the DELETE method.
+        It should remove a file specified by the URL path.
+        Send 404 Not Found if not found by the path.
+        :param request: The request object.
+        :return: An HTTP response to send back to the client.
+        """
+        if request.method != HttpMethod.DELETE:
+            raise f"DELETE request handler cannot handle request of {request.method}"
+
+        # TODO: implement
+        ...
+
+    def __handle_unsupported_request(self, request: HttpRequest) -> HttpResponse:
+        """
+        Handles an HTTP request of an unexpected method.
+        :param request: The request object.
+        :return: An HTTP response to send back to the client.
+        """
+
+        # TODO: implement
+        ...
+
+    def handle(self):
+        """
+        Handles a connection.
+        :param conn: The connection instance.
+        :param address: The (Host, Port) of the peer.
+        """
+        request = self.__receive_request(self.connection)
+
+        match request.method:
+            case HttpMethod.GET:
+                self.__handle_get_request(request)
+            case HttpMethod.PUT:
+                self.__handle_put_request(request)
+            case HttpMethod.HEAD:
+                self.__handle_head_request(request)
+            case HttpMethod.POST:
+                self.__handle_post_request(request)
+            case HttpMethod.DELETE:
+                self.__handle_delete_request(request)
+            case _:
+                self.__handle_unsupported_request(request)
+
+
 class ShoddyServer(object):
     """
     HTTP Server class responsible for binding to a socket and listening for connections.
     """
 
-    def __init__(self, host: str, port: int, start=False):
+    def __init__(self, host: str, port: int, *, timeout=5):
         """
         Creates a new shoddy server to listen for connections.
         :param host: The IP address to use for the server.
         :param port: The port to use for the server.
-        :param start: Whether to start the server immediately
+        :param timeout: The number of seconds to wait for receiving before sending a timeout response.
         """
         self.host = host
         self.port = port
         self.soc = socket(family=AF_INET, type=SOCK_STREAM)
-        self.running = start
-
-        if start:
-            self.start()
-
-    def __handle_connection(self, connection: socket, address: HostAndPort):
-        """
-        Handles a connection.
-        :param connection: The connection instance.
-        :param address: The (Host, Port) of the peer.
-        """
-        logging.info(f"Server - Connected by: {address}")
-
-        connected = True
-        while connected:
-            data = connection.recv(1024)
-
-            if not data:
-                connected = False
-
-            s = data.decode()
-
-            logging.info(f"Received\n'{s}'\nfrom {address}")
-
-            content = ""
-
-            with open("test.html", "r") as file:
-                content = file.read()
-
-            headers = {
-                "Content-Length": len(content)
-            }
-
-            status = 200
-
-            response = f"HTTP/1.1 {status}" + "\n".join(
-                f"{key}: {value}" for key, value in headers.items()) + "\n\n" + content
-
-            connection.send(response.encode())
-
-            if s == 'stop':
-                connected = False
-                self.running = False
+        self.timeout = timeout
 
     def start(self):
         """
@@ -227,13 +348,13 @@ class ShoddyServer(object):
         self.soc.bind((self.host, self.port))
         self.soc.listen(5)
 
-        self.running = True
+        running = True
 
-        while self.running:
-            self.__handle_connection(*self.soc.accept())
+        while running:
+            ConnectionHandler(*self.soc.accept()).handle()
 
         self.soc.close()
-        self.running = False
+        running = False
 
 
 def main():
@@ -242,10 +363,10 @@ def main():
         level=logging.INFO
     )
 
-    HOST = "127.0.0.1"
-    PORT = 80
-    logging.info("Starting server")
-    server = ShoddyServer(HOST, PORT, True)
+    host = "127.0.0.1"
+    port = 80
+    logging.info("Creating server")
+    ShoddyServer(host, port).start()
     logging.info("Stopped server")
 
 
--- test.py
+++ test.py
@@ -11,25 +11,26 @@ class HttpStatusTest(unittest.TestCase):
 
 
 class HttpRequestObjectTest(unittest.TestCase):
-    raw = ("GET / HTTP/1.1\n"
-           "Host: 127.0.0.1\n"
-           "Connection: keep-alive\n"
-           "sec-ch-ua: \"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"\n"
-           "sec-ch-ua-mobile: ?0\n"
-           "sec-ch-ua-platform: \"Windows\"\n"
-           "Upgrade-Insecure-Requests: 1\n"
+    raw = ("GET / HTTP/1.1\r\n"
+           "Host: 127.0.0.1\r\n"
+           "Connection: keep-alive\r\n"
+           "sec-ch-ua: \"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"\r\n"
+           "sec-ch-ua-mobile: ?0\r\n"
+           "sec-ch-ua-platform: \"Windows\"\r\n"
+           "Upgrade-Insecure-Requests: 1\r\n"
            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
-           "like Gecko) Chrome/107.0.0.0 Safari/537.36\n"
+           "like Gecko) Chrome/107.0.0.0 Safari/537.36\r\n"
            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,"
-           "image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\n"
-           "Sec-Fetch-Site: none\n"
-           "Sec-Fetch-Mode: navigate\n"
-           "Sec-Fetch-User: ?1\n"
-           "Sec-Fetch-Dest: document\n"
-           "Accept-Encoding: gzip, deflate, br\n"
-           "Accept-Language: en-CA,en;q=0.9,zh-CN;q=0.8,zh;q=0.7\n"
-           "dnt: 1\n"
-           "\n")
+           "image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n"
+           "Sec-Fetch-Site: none\r\n"
+           "Sec-Fetch-Mode: navigate\r\n"
+           "Sec-Fetch-User: ?1\r\n"
+           "Sec-Fetch-Dest: document\r\n"
+           "Accept-Encoding: gzip, deflate, br\r\n"
+           "Accept-Language: en-CA,en;q=0.9,zh-CN;q=0.8,zh;q=0.7\r\n"
+           "dnt: 1\r\n"
+           "\r\n"
+           "")
 
     def test_construction(self):
         headers = {
@@ -61,20 +62,20 @@ class HttpRequestObjectTest(unittest.TestCase):
 
 
 class HttpResponseObjectTest(unittest.TestCase):
-    raw = ("HTTP/1.1 200 OK\n"
-           "Date: Sun, 04 Dec 2022 07:23:10 GMT\n"
-           "Content-Type: text/html\n"
-           "Content-Length: 2335\n"
-           "Connection: keep-alive\n"
-           "Content-Encoding: gzip\n"
-           "Last-Modified: Fri, 08 Apr 2022 16:54:52 GMT\n"
-           "Accept-Ranges: bytes\n"
-           "ETag: \"0d6835d694bd81:0\"\n"
-           "Vary: Accept-Encoding\n"
-           "Server: Microsoft-IIS/10.0\n"
-           "X-Powered-By: ASP.NET\n"
-           "SN: EC2AMAZ-BSL60ON\n"
-           "\n")
+    raw = ("HTTP/1.1 200 OK\r\n"
+           "Date: Sun, 04 Dec 2022 07:23:10 GMT\r\n"
+           "Content-Type: text/html\r\n"
+           "Content-Length: 2335\r\n"
+           "Connection: keep-alive\r\n"
+           "Content-Encoding: gzip\r\n"
+           "Last-Modified: Fri, 08 Apr 2022 16:54:52 GMT\r\n"
+           "Accept-Ranges: bytes\r\n"
+           "ETag: \"0d6835d694bd81:0\"\r\n"
+           "Vary: Accept-Encoding\r\n"
+           "Server: Microsoft-IIS/10.0\r\n"
+           "X-Powered-By: ASP.NET\r\n"
+           "SN: EC2AMAZ-BSL60ON\r\n"
+           "\r\n")
 
     def test_construction(self):
         headers = {
@@ -95,6 +96,9 @@ class HttpResponseObjectTest(unittest.TestCase):
         expected_string = self.raw
 
         self.assertEqual(expected_string, HttpResponse(headers=headers).to_raw())
+
+
+
 
 
 if __name__ == "__main__":
